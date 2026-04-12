015b22a1e2770fe1b35f33bb9521501acce37ac6
Shaiekh Zayed Bin Hasan <80541095+frost23z@users.noreply.github.com>
2026-04-12 18:06:41 +0600

    feat(database): add new indices for receipts and audit events (#5)

--- android-app/app/src/main/java/me/zayedbinhasan/android_app/data/local/db/LocalDatabaseFactory.kt
+++ android-app/app/src/main/java/me/zayedbinhasan/android_app/data/local/db/LocalDatabaseFactory.kt
@@ -5,11 +5,13 @@ import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import me.zayedbinhasan.data.Database
 
 object LocalDatabaseFactory {
+    private const val DEFAULT_DATABASE_NAME = "digital_delta.db"
+
     fun create(context: Context): Database {
         val driver = AndroidSqliteDriver(
             schema = Database.Schema,
             context = context,
-            name = "digital_delta.db",
+            name = DEFAULT_DATABASE_NAME,
         )
         return Database(driver)
     }
