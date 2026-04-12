e855a496f73c72106859e40f041bae573ad1b7e4
frost23z <80541095+frost23z@users.noreply.github.com>
2026-04-12 19:05:46 +0600

    feat(android): add basic UI scaffold with navigation, icons, and placeholders

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.sqldelight.android.driver)
     implementation(libs.sqldelight.coroutines.extensions)
     testImplementation(libs.junit)
